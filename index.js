c8753212abe8af78970180c5c45b5ad8f6ce46a2
rafiqmia65 <rafiqdeveloper4465@gmail.com>
2025-06-17 02:27:44 +0600

    Final Update

--- index.js
+++ index.js
@@ -64,9 +64,6 @@ const verifyTokenEmail = (req, res, next) => {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-
     const roomsCollection = client.db("hoteleo").collection("rooms");
 
     app.post("/rooms", async (req, res) => {
@@ -310,15 +307,7 @@ async function run() {
             : "Booking not found or already cancelled",
       });
     });
-
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
   } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
   }
 }
 run().catch(console.dir);
