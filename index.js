61e50e69c8fcbbddc69d9f81b1d5751981c7898c
rafiqmia65 <rafiqdeveloper4465@gmail.com>
2025-06-16 18:31:41 +0600

    Jwt Authentication added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 require("dotenv").config();
 const app = express();
 require("dotenv").config();
+const admin = require("firebase-admin");
 const port = process.env.PORT || 3000;
 
 // Middle Ware
@@ -20,6 +21,46 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   },
 });
+
+// token Convert
+
+const decoded = Buffer.from(process.env.FB_SERVICE_KEY, "base64").toString(
+  "utf8"
+);
+const serviceAccount = JSON.parse(decoded);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+// Verify Token
+
+const verifyFireBaseToken = async (req, res, next) => {
+  const authHeader = req.headers?.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = await admin.auth().verifyIdToken(token);
+
+    req.decoded = decoded;
+
+    next();
+  } catch (error) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+};
+
+// Verify Email
+const verifyTokenEmail = (req, res, next) => {
+  if (req.query.email !== req.decoded.email) {
+    return res.status(403).send({ message: "forbidden access" });
+  }
+  next();
+};
 
 async function run() {
   try {
@@ -51,6 +92,8 @@ async function run() {
       const result = await roomsCollection.find(filter).toArray();
       res.send(result);
     });
+
+    // Top Rated Room
 
     app.get("/top-rated-room", async (req, res) => {
       const topRooms = await roomsCollection
@@ -100,12 +143,16 @@ async function run() {
       res.send(topRooms);
     });
 
+    // Room Details
+
     app.get("/rooms/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await roomsCollection.findOne(query);
       res.send(result);
     });
+
+    // Booking Process
 
     app.patch("/book-room/:id", async (req, res) => {
       const roomId = req.params.id;
@@ -124,29 +171,38 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/my-bookings", async (req, res) => {
-      const { email } = req.query;
+    // My Bookings Rooms
 
-      const rooms = await roomsCollection.find({}).toArray();
+    app.get(
+      "/my-bookings",
+      verifyFireBaseToken,
+      verifyTokenEmail,
+      async (req, res) => {
+        const { email } = req.query;
 
-      const userBookings = [];
+        const rooms = await roomsCollection.find({}).toArray();
 
-      rooms.forEach((room) => {
-        room.bookedDates?.forEach((booking) => {
-          if (booking.email === email) {
-            userBookings.push({
-              bookingId: booking._id,
-              roomId: room._id,
-              title: room.title,
-              image: room.image,
-              date: booking.date,
-            });
-          }
+        const userBookings = [];
+
+        rooms.forEach((room) => {
+          room.bookedDates?.forEach((booking) => {
+            if (booking.email === email) {
+              userBookings.push({
+                bookingId: booking._id,
+                roomId: room._id,
+                title: room.title,
+                image: room.image,
+                date: booking.date,
+              });
+            }
+          });
         });
-      });
 
-      res.send(userBookings);
-    });
+        res.send(userBookings);
+      }
+    );
+
+    // Total Review get
 
     app.get("/latest-reviews", async (req, res) => {
       const rooms = await roomsCollection.find().toArray();
@@ -167,6 +223,8 @@ async function run() {
 
       res.send(allReviews);
     });
+
+    // Review Added
 
     app.patch("/review/:roomId", async (req, res) => {
       const { roomId } = req.params;
@@ -197,6 +255,8 @@ async function run() {
       });
     });
 
+    // Booking Date Update
+
     app.patch("/booking-date-update", async (req, res) => {
       const { roomId, bookingId, newDate } = req.body;
 
@@ -221,6 +281,8 @@ async function run() {
             : "Booking not found or already updated",
       });
     });
+
+    // Booking Room Cancel
 
     app.delete("/booking-cancel", async (req, res) => {
       const { bookingId, roomId } = req.body;
